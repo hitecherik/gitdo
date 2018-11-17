c7b16701f14b33f9ed2dcd4a6292c406fe539e14
Alexander Nielsen <hitecherik@gmail.com>
2018-11-17 15:34:35 +0000

    Execute commands

--- execute.js
+++ execute.js
@@ -1,3 +1,23 @@
+const exec = require("child_process").exec;
+
+function _execute(commands, callback) {
+  const command = commands.join(" && ");
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`There was an error: ${error.toString()}\n`);
+    }
+
+    console.log("\n    " + stdout.replace("\n", "\n    "));
+
+    if (stderr.length > 0) {
+      console.error(stdout.replace("\n", "\n    "));
+    }
+
+    callback();
+  });
+}
+
 function execute(commands, rl, confirm = true) {
   console.log("\nThese are the commands we've generated: \n");
 
@@ -10,16 +30,17 @@ function execute(commands, rl, confirm = true) {
   rl.question("Should we execute these commands? [Y/n] ", answer => {
     const choice = answer.length == 0 ? 'y' : answer.toLowerCase()[0];
 
-    switch(choice.toLowerCase()[0]) {
+    switch(choice) {
       case 'y':
         console.log("Executing...");
-        break;
+        _execute(commands, () => rl.close());
+        return;
 
       case 'n':
         console.log("Cancelling...");
         break;
 
-      case 'default':
+      default:
         console.log(`We couldn't understand "${answer}". Cancelling...`);
         break;
     }
